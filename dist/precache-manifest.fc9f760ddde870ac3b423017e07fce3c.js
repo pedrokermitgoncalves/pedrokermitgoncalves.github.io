c5617cba37a2819bd818a8b5432cc4c2bb93b131
Pedro "Kermit" Gonçalves <pedro.goncalves@talkdesk.com>
2020-09-21 14:44:54 +0100

    feat: 🎸 add dist folder

---
+++ dist/precache-manifest.fc9f760ddde870ac3b423017e07fce3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e456fc8500d2e9b5cd9aa32020f91935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d0ab039c623f46785d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6a159f680e3ef57700b",
+    "url": "/static/js/2.f3496f1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3496f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d0ab039c623f46785d",
+    "url": "/static/js/main.261e3481.chunk.js"
+  },
+  {
+    "revision": "bcc29d1a8a34e95f0295",
+    "url": "/static/js/runtime-main.d11ebf7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
